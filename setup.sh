66d9a174c7ab3357866c9672d543bdcf61f1e2b7
Ryan Williams <steelsavant@gmail.com>
2022-06-08 13:33:32 -0600

    update build scripts

--- setup.sh
+++ setup.sh
@@ -1,10 +1,17 @@
 # usage: ./setup.sh (OPT)SRC_BRANCH_NAME
 # example (origin): ./setup.sh fix/something
-# example (upstream): ./setup.sh "-b main upstream/main"
 MASTER="main"
 BRANCH="${1:-$MASTER}"
+GOOGLE_WEBRTC_DIR="./google_webrtc_android"
+
 
 cd ..
+if [ ! -d $GOOGLE_WEBRTC_DIR ]
+then
+    mkdir $GOOGLE_WEBRTC_DIR
+fi
+cd $GOOGLE_WEBRTC_DIR
+
 if [ ! -d "./depot_tools" ]
 then
     echo "cloning depot tools repository"
@@ -15,18 +22,19 @@ fi
 cd ./depot_tools
 export PATH=$PATH:$(pwd)
 cd ..
-if [ ! -d "./google_webrtc_android" ]
+if [ ! -d "./src" ]
 then
+    mkdir ./src 
+    cd ./src
     # export GIT_DISCOVERY_ACROSS_FILESYSTEM=1 #helps with issues running in wsl
-    echo "cloning modified webrtc repository with branch ${BRANCH}"
-    git clone https://github.com/Photorithm/google_webrtc_android.git 
-    cd ./google_webrtc_android
+    echo "cloning modified webrtc repository"
+    git clone -b $BRANCH https://github.com/Photorithm/google_webrtc_android.git .
     git remote add upstream https://github.com/webrtc-sdk/webrtc
-    git config checkout.defaultRemote origin
+    # git config checkout.defaultRemote origin
 else 
     echo "cloned source already exists; switching to branch ${BRANCH}"
-    cd ./google_webrtc_android
+    cd ./src
+    git fetch upstream
+    git checkout $BRANCH
 fi
-git fetch upstream
-git checkout $BRANCH
 echo "finished setup"
