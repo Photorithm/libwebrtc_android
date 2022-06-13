2fa5d3a49483e813be9f6c9ff996917d4296ac07
Ryan Williams <steelsavant@gmail.com>
2022-06-13 09:22:22 -0600

    update scripts (again)

--- build.sh
+++ build.sh
@@ -2,20 +2,20 @@ cd ../google_webrtc_android/depot_tools
 export PATH=$PATH:$(pwd)
 cd ../src
 echo "building aar file... please wait"
-./tools_webrtc/android/build_aar.py --verbose
-# cd ../..
-# echo "removing old build"
-# rm ./google_webrtc_android/google-webrtc-v3.aar
-# rm -r ./libwebrtc_android/com/aar/app/google-webrtc/*
-# cp ./google_webrtc_android/libwebrtc.aar ./google_webrtc_android/google-webrtc-v3.aar
-# mvn install:install-file -DgroupId=com.aar.app -DartifactId=google-webrtc -Dversion=v3 -Dfile=./google_webrtc_android/src/google-webrtc-v3.aar -Dpackaging=aar -DgeneratePom=true -Dlocalrepository=./libwebrtc_android/com/aar/app/google-webrtc/v3 -DcreateChecksum=true
-# echo "copying maven files to repository"
-# cp -R ~/.m3/repository/com/aar/app/google-webrtc/* ./libwebrtc_android/com/aar/app/google-webrtc/
-# cd ./libwebrtc_android
-# echo "uploading files to git"
-# git checkout repo 
-# git add .
-# git commit -m "update aar"
-# git push
+vpython ./tools_webrtc/android/build_aar.py --verbose
+cd ../..
+echo "removing old build"
+rm ./google_webrtc_android/google-webrtc-v3.aar
+rm -r ./libwebrtc_android/com/aar/app/google-webrtc/*
+cp ./google_webrtc_android/libwebrtc.aar ./google_webrtc_android/google-webrtc-v3.aar
+mvn install:install-file -DgroupId=com.aar.app -DartifactId=google-webrtc -Dversion=v3 -Dfile=./google_webrtc_android/src/google-webrtc-v3.aar -Dpackaging=aar -DgeneratePom=true -Dlocalrepository=./libwebrtc_android/com/aar/app/google-webrtc/v3 -DcreateChecksum=true
+echo "copying maven files to repository"
+cp -R ~/.m3/repository/com/aar/app/google-webrtc/* ./libwebrtc_android/com/aar/app/google-webrtc/
+cd ./libwebrtc_android
+echo "uploading files to git"
+git checkout repo 
+git add .
+git commit -m "update aar"
+git push
 echo "finished build"
 
--- setup.sh
+++ setup.sh
@@ -1,40 +1,47 @@
-# usage: ./setup.sh (OPT)SRC_BRANCH_NAME
-# example (origin): ./setup.sh fix/something
-MASTER="main"
-BRANCH="${1:-$MASTER}"
-GOOGLE_WEBRTC_DIR="./google_webrtc_android"
-
-
 cd ..
-if [ ! -d $GOOGLE_WEBRTC_DIR ]
-then
-    mkdir $GOOGLE_WEBRTC_DIR
-fi
-cd $GOOGLE_WEBRTC_DIR
-
 if [ ! -d "./depot_tools" ]
 then
     echo "cloning depot tools repository"
     git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
 else 
-    echo "depot tools already exists; skipping clone"
+    echo "depot tools already exist; skipping clone"
 fi
 cd ./depot_tools
 export PATH=$PATH:$(pwd)
 cd ..
-if [ ! -d "./src" ]
+if [ ! -d "./google_webrtc_android" ]
 then
-    mkdir ./src 
+    mkdir ./google_webrtc_android 
+    cd ./google_webrtc_android
+    echo "fetching webrtc_android from google; this may take multiple hours"
+    fetch --nohooks webrtc_android
     cd ./src
-    # export GIT_DISCOVERY_ACROSS_FILESYSTEM=1 #helps with issues running in wsl
-    echo "cloning modified webrtc repository"
-    git clone -b $BRANCH https://github.com/Photorithm/google_webrtc_android.git .
-    git remote add upstream https://github.com/webrtc-sdk/webrtc
-    # git config checkout.defaultRemote origin
+    git checkout -B m93 25e3fd53a79bfdb2bd647ee3a199eb9c3a71d271 
+    cd ..
+    echo "syncing gclient"
+    gclient sync
+    cd ./src
+    rm -r -f .git
+    ./build/install-build-deps.sh
+    cd ../../
 else 
-    echo "cloned source already exists; switching to branch ${BRANCH}"
-    cd ./src
-    git fetch upstream
-    git checkout $BRANCH
+    echo "project exists; skipping fetch"
+    cd ./google_webrtc_android/src
 fi
+
+echo "removing old files"
+[ -d "./tmp" ] && rm -r -f ./tmp
+
+echo "cloning modified webrtc repository"
+git clone https://github.com/Photorithm/google_webrtc_android.git tmp
+cd tmp 
+git remote add upstream https://webrtc.googlesource.com/src
+cd ..
+echo "installing build dependencies"
+
+echo "copying necessary files; this may take several minutes"
+shopt -s dotglob nullglob
+cp -r ./tmp/* .
+echo "cleaning up temporary files; this may take several minutes"
+rm -r -f ./tmp
 echo "finished setup"
