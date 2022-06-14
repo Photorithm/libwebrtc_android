579d36db0938cb03487c30ae40be7f3569a8e22e
Ryan Williams <steelsavant@gmail.com>
2022-06-13 18:47:54 -0600

    update setup script

--- setup.sh
+++ setup.sh
@@ -14,26 +14,25 @@ then
     mkdir ./google_webrtc_android 
     cd ./google_webrtc_android
     echo "fetching webrtc_android from google; this may take multiple hours"
-    fetch --nohooks webrtc_android
+    fetch --nohooks -no-history webrtc_android
     cd ./src
     git checkout -B m93 25e3fd53a79bfdb2bd647ee3a199eb9c3a71d271 
     cd ..
     echo "syncing gclient"
-    gclient sync
+    gclient sync --revision src@25e3fd53a79bfdb2bd647ee3a199eb9c3a71d271
     cd ./src
-    rm -r -f .git
     ./build/install-build-deps.sh
+    rm -r -f .git
     cd ../../
 else 
     echo "project exists; skipping fetch"
-    cd ./google_webrtc_android/src
 fi
 
 echo "removing old files"
 [ -d "./tmp" ] && rm -r -f ./tmp
 
 echo "cloning modified webrtc repository"
-git clone https://github.com/Photorithm/google_webrtc_android.git tmp
+git clone -b main https://github.com/Photorithm/google_webrtc_android.git tmp
 cd tmp 
 git remote add upstream https://webrtc.googlesource.com/src
 cd ..
